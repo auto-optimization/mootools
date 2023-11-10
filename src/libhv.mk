a27b097a134429ae9e13c6b5c8e879f734911412
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-11-10 12:08:26 +0000

    fix mkdir

--- src/libhv.mk
+++ src/libhv.mk
@@ -4,7 +4,7 @@ LIBHV_HDRS    = hv.h
 LIBHV_OBJS    = $(LIBHV_SRCS:.c=.o)
 HV_LIB     = fpli_hv.a
 
-$(HV_LIB): $(LIBHV_OBJS) libmootools.o
+$(HV_LIB): $(LIBHV_OBJS) libutil.o
 	@$(RM) $@
 	$(QUIET_AR)$(AR) rcs $@ $^
 
