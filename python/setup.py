f3f1b6b0a428df5f1177a776ad7fae76ad1e7bfe
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-11-12 14:33:12 +0000

    fix python flags

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(
-    cffi_modules=["src/moocore/build_c_moocore.py:ffibuilder"],
+    cffi_modules=["src/moocore/_ffi_build.py:ffibuilder"],
     package_data={"": ["*.h", "*.c"]},
 )
